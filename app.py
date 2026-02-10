ff8bda9ce4ec73e4e74b0d8f1266962abb2ddc5a
Zlati839 <zlatipenev7@gmail.com>
2026-02-10 14:19:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import streamlit as st
-st.title("my 1st project")
+st.title("Math test")
 name = st.text_input("Whats your name ")
 if name :
   st.write(f"hello {name}")
   
-color = st.text_input("Whats your favorite color ")
-if color :
-  st.write(f"{color} is a nice color")
+color = st.text_input("Whats 2+2 ")
+if color = 4 :
+  st.success(f"Correct!!")
+else:
+   st.warning(f"Wrong")
+Subject = st.text_input("Whats 10/2 ")
+if Subject = 5 :
+  st.success(f"Correct!!")
+else:
+   st.warning(f"Wrong")
 
-Subject = st.text_input("Whats your favorite subject ")
-if Subject :
-  st.write(f"{Subject} , intresting")
+Hobby = st.text_input("Whats 4(100*2) ?")
+if Hobby = 5 :
+  st.success(f"Correct!!")
+else:
+ st.warning(f"Wrong")
 
-Hobby = st.text_input("How about Hobies ?")
-if Hobby :
-  st.write(f"{Hobby} Sounds Fun :D")
-
-Age = st.text_input("Whats your age  ?")
-if Age :
-  st.write(f"{Age} , nice ")
+Age = st.text_input("Whats 10000000000 - 1  ?")
+if Age = 9999999999 :
+  st.success(f"Correct!!")
+else:
+ st.warning(f"Wrong")
