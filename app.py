3d8738d644bba729df06fb59b3178cfe20b0e3af
Zlati839 <zlatipenev7@gmail.com>
2026-02-10 14:21:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,24 @@ if name :
   st.write(f"hello {name}")
   
 color = st.text_input("Whats 2+2 ")
-if color = 4 :
+if color == 4 :
   st.success(f"Correct!!")
 else:
    st.warning(f"Wrong")
 Subject = st.text_input("Whats 10/2 ")
-if Subject = 5 :
+if Subject == 5 :
   st.success(f"Correct!!")
 else:
    st.warning(f"Wrong")
 
 Hobby = st.text_input("Whats 4(100*2) ?")
-if Hobby = 5 :
+if Hobby == 800 :
   st.success(f"Correct!!")
 else:
  st.warning(f"Wrong")
 
 Age = st.text_input("Whats 10000000000 - 1  ?")
-if Age = 9999999999 :
+if Age == 9999999999 :
   st.success(f"Correct!!")
 else:
  st.warning(f"Wrong")
