2aa05c92dea0bfc7cc7c196e06e42caac1c0c6db
Zlati839 <zlatipenev7@gmail.com>
2026-02-10 14:09:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ name = st.text_input("Whats your name ")
 if name :
   st.write(f"hello {name}")
   
-color = st.text_input("Whats your favorite ")
+color = st.text_input("Whats your favorite color ")
 if color :
   st.write(f"{color} is a nice color")
+
+Subject = st.text_input("Whats your favorite subject ")
+if Subject :
+  st.write(f"{Subject} , intresting")
